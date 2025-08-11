7e4741468e17ee24341a92ccdc470ea1343c84ad
Josh <joboshua15@gmail.com>
2025-08-11 10:53:36 -0600

    pre and co requisites scraped

--- scraper/clear_course.py
+++ scraper/clear_course.py
@@ -1,9 +1,9 @@
 from __future__ import annotations
 
 class ClearCourse:
-    def __init__(self, href: str, course_name: str = ""):
-        self.href_scraped = False
-        self.href: str = href
+    def __init__(self, route: str, course_name: str = ""):
+        self._scraped = False
+        self.route: str = route
         self.course_name: str = course_name
         self.co_reqs: set[ClearCourse] = set()
         self.pre_reqs: set[ClearCourse] = set()
@@ -14,28 +14,31 @@ class ClearCourse:
             "co"      : self.co_reqs
         }
 
+    def is_scraped(self):
+        return self._scraped
+
     def set_scraped(self):
-        self.href_scraped = True
+        self._scraped = True
 
     def add_requisite(self, course_to_add: ClearCourse, _which_list = "pre") -> None:
         self._requisite_lists[_which_list].add(course_to_add)
 
     def __iter__(self):
-        yield "href", self.href
+        yield "route", self.route
         yield "course_name", self.course_name
         # Convert requisite sets into lists of hrefs for serialization
         yield "requisites", {
-            k: [c.href for c in v] for k, v in self._requisite_lists.items()
+            k: [c.route for c in v] for k, v in self._requisite_lists.items()
         }
 
 
     def __str__(self) -> str:
         this_string = str()
-        this_string += f"this href - {self.href}\n"
+        this_string += f"this route - {self.route}\n"
         for key, value in self._requisite_lists.items():
             this_string += f"{key}: [\n"
-            for v in sorted(list(value), key=lambda c: c.href):
+            for v in sorted(list(value), key=lambda c: c.route):
                 this_string += f"\tcourse name - {v.course_name}\n"
-                this_string += f"\thref - {v.href}\n"
+                this_string += f"\troute - {v.route}\n"
             this_string += "]\n\n"
         return this_string
--- scraper/scraper.py
+++ scraper/scraper.py
@@ -3,8 +3,12 @@ import requests as req
 from bs4 import BeautifulSoup as bs
 from bs4 import Tag, ResultSet
 
+#region constants
+PARSER = 'html.parser'
+CS_CATALOG_BASE_URL = "https://neumont.smartcatalogiq.com/en/2024-2025/2024-2025/undergraduate-programs/undergraduate-program-overview"
+INDIVIDUAL_COURSE_BASE_URL = "https://neumont.smartcatalogiq.com"
 
-ALL_CATALOG_THINGIES = [
+ALL_CATALOG_ROUTES = [ # minimum number to get all courses
     "/bachelor-of-science-in-computer-science/",
     "/applied-artificial-intelligence-and-data-engineering/",
     "/artificial-intelligence-engineering/",
@@ -13,41 +17,50 @@ ALL_CATALOG_THINGIES = [
     # "/software-engineering/",
     # "/associates-of-science-in-software-development/"
 ]
+#endregion
 
-CS_CATALOG_BASE_URL = "https://neumont.smartcatalogiq.com/en/2024-2025/2024-2025/undergraduate-programs/undergraduate-program-overview"
-
-INDIVIDUAL_COURSE_BASE_URL = "https://neumont.smartcatalogiq.com"
-
-course_hrefs: set[str] = set()
-for route in ALL_CATALOG_THINGIES:
+course_routes: set[str] = set()
+for route in ALL_CATALOG_ROUTES:
     request = req.get(f"{CS_CATALOG_BASE_URL}{route}")
-    html = request.text
+    degree_html = request.text
+    
+    soup = bs(degree_html, PARSER)
+    
+    course_codes: ResultSet[Tag] = soup.find_all(class_="sc-courselink")
+    
+    for course_code in course_codes:
+        course_routes.add(course_code['href'])
+    
+    print(len(course_routes))
 
-    soup = bs(html, 'html.parser')
+course_routes = sorted(list(course_routes))
 
-    tables: ResultSet[Tag] = soup.find_all("table")
+courses = [ClearCourse(course) for course in course_routes]
 
-    # print(tables)
-
-    for table in tables:
-        thingy: ResultSet[Tag] = table.find_all(class_="sc-coursenumber")
-        if not thingy:
+def find_requisite_instance(course: ClearCourse, course_href, which_req_type):
+    global courses
+    for c in courses:
+        if c.route != course_href:
             continue
-        for thing in thingy:
-            course_hrefs.add(thing.find(class_="sc-courselink")['href'])
-    print(len(course_hrefs))
+        course.add_requisite(c, which_req_type)
+        break
 
-course_hrefs = sorted(list(course_hrefs))
+def scrape_requisites(soup: bs, which_class: str, which_req_type: str):
+    requisites: Tag = soup.find(class_=which_class)
+    requisite_routes = requisites.find_all("a")
+    for route in requisite_routes:
+        find_requisite_instance(course, route["href"], which_req_type)
 
-# soupy = bs(req.get(f"https://neumont.smartcatalogiq.com{course_hrefs[0]}").text, "html.parser")
 
-courses = [ClearCourse(course) for course in course_hrefs]
-
-for course in courses[0:1]:
-    request = req.get(f"{INDIVIDUAL_COURSE_BASE_URL}{course.href}")
+for course in courses:
+    request = req.get(f"{INDIVIDUAL_COURSE_BASE_URL}{course.route}")
     course.set_scraped()
-    soup = bs(request.text, 'html.parser')
-    print(soup.prettify())
+    course_html = request.text
+    soup = bs(course_html, PARSER)
+    scrape_requisites(soup, "sc_prereqs", "pre")
+    scrape_requisites(soup, "sc_coreqs", "co")
 
-# for course in courses:
+# for course in courses[0:10]:
+#     print(course)
+# for course in course_routes:
 #     print(course)
