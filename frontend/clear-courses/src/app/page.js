27ce38ebfcabae1a87987e5ca15f3055c90ecbc1
RedSousuke <rmeeks@student.neumont.edu>
2025-08-18 18:46:48 -0600

    Initial Commit
    Some tweaks to the styling of the frontend interface

--- frontend/clear-courses/src/app/page.js
+++ frontend/clear-courses/src/app/page.js
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import coursesData from "./public/Data/courses.json";
+import './globals.css'
 
 export default function Home() {
   const courses = Object.values(coursesData);
@@ -7,15 +8,18 @@ export default function Home() {
   return (
     <div>
       <h1>Courses</h1>
-      <ul>
-        {courses.map(course => (
-          <li key={course.route}>
-            <Link href={`/${encodeURIComponent(course.route.split('/').pop())}`}>
-              {course.course_name}
-            </Link>
-          </li>
-        ))}
-      </ul>
+      <flex class="course_table">
+         <table>
+            {courses.map(course => (
+               <tr class={course.course_code.substring(0, 3)} key={course.route}>
+                  <td class="course"><Link href={`/${encodeURIComponent(course.route.split('/').pop())}`}>
+                        {course.course_name}
+                        </Link></td>
+                  <td>{course.course_code}</td>
+               </tr>
+            ))}
+         </table>
+      </flex>
     </div>
   );
 }
