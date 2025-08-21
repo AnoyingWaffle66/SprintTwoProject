81c007b8d7bff1f1f5ce88528f4d684b0954629e
RedSousuke <rmeeks@student.neumont.edu>
2025-08-21 11:28:55 -0600

    updated page generation and styling

--- frontend/clear-courses/src/app/Components/CourseDetail.js
+++ frontend/clear-courses/src/app/Components/CourseDetail.js
@@ -1,15 +1,21 @@
-import Link from 'next/link';
+﻿import Link from 'next/link';
 import coursesData from '../public/Data/courses.json';
 
 export default function CourseDetail({ course }) {
     const renderLinks = (list) => {
-        return list.map(r => {
-            const courseObj = coursesData[r];
-            const courseName = courseObj ? courseObj.course_name : r.split('/').pop();
-            const slug = r.split('/').pop();
+        if (list === undefined || list.length == 0) {
             return (
-                <li key={r}>
-                    <Link href={`/${slug}`}>{courseName}</Link>
+                <li key="list">
+                    <p>None</p>
+                </li>
+            );
+        }
+        return list.map(courseObj => {
+            const courseCode = courseObj.course_code;
+            const courseName = courseObj.course_name;
+            return (
+                <li key={courseObj}>
+                    <Link href={`/${courseCode}`}>{courseCode} - {courseName}</Link>
                 </li>
             );
         });
@@ -17,14 +23,14 @@ export default function CourseDetail({ course }) {
 
     return (
         <div>
-            <h1>{course.course_name}</h1>
-            <h2>Pre-requisites</h2>
+            <h1 id="title">{course.course_name}</h1>
+            <h2 class="section">Pre-requisites</h2>
             <ul>{renderLinks(course.requisites.pre)}</ul>
-            <h2>Pre-for</h2>
+            <h2 class="section">Pre-for</h2>
             <ul>{renderLinks(course.requisites['pre-for'])}</ul>
-            <h2>Co-requisites</h2>
+            <h2 class="section">Co-requisites</h2>
             <ul>{renderLinks(course.requisites.co)}</ul>
-            <Link href="/">Back to Courses</Link>
+            <Link href="/">⇦ Back to Courses</Link>
         </div>
     );
 }
